03627b015a786087e8ab5603728db9ee0878ae93
ManishGoti15 <121253758+ManishGoti15@users.noreply.github.com>
2022-12-24 21:07:44 +0530

    Add files via upload

---
+++ TOPS--C,C++/Module  3.1/01__information.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Name        : Manish Goti\n");
+    printf("Birth Date  : 10-May-2003\n");
+    printf("Age         : 20 Years\n");
+    printf("Address     : Surat\n");
+
+    return 0;
+}
---
+++ TOPS--C,C++/Module  3.1/04__simple_interest.c
@@ -0,0 +1,10 @@
+int main()
+{
+    float P = 12, R = 55, T = 20;
+
+    float SI = (P * T * R) / 100;
+
+    printf("Simple Interest = %f\n", SI);
+
+    return 0;
+}
